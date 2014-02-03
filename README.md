aad8bee0e004ac4f68ed8670f4f70a54aac53fee
Adam Pearson <adam@substantial.com>
2014-02-03 14:40:46 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+XCode Snippets
+=================
+
+## Included:
+
+* Kiwi snippets (beforeEach, it, etc.)
+* Dealloc logging (useful for tracking down leaks)
+
+## Installation / Updates:
+
+Run the install script:
+    ./install.sh
---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets
+cp -R ./snippets/ ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+echo "Done copying templates to ~/Library/Developer/Xcode/UserData/CodeSnippets"
